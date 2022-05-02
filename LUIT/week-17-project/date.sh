6ed420a83c267ee17ffb11d31a2b25b24c90aaeb
Deshone H <deshonehenry@gmail.com>
2022-05-02 13:04:00 -0400

    Update date.sh

--- LUIT/week-17-project/date.sh
+++ LUIT/week-17-project/date.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #.... Initialization steps
-echo Image Built: $(date "+%Y-%m-%d %H:%M:%S") > build-date.txt
+#$(date "+%Y-%m-%d %H:%M:%S:%Z") ##UTC TZ
+echo Image Built: $(TZ=EST5EDT date "+%Y-%m-%d %H:%M:%S:%Z") > build-date.txt
 echo $(cat build-date.txt)
 #.... More initialization steps
 # run the command
