8cf0ddae3f1413c1f792d2344a4bbe451a4ad38d
Deshone H <deshonehenry@gmail.com>
2022-05-02 13:07:35 -0400

    Update date.sh

--- LUIT/week-17-project/date.sh
+++ LUIT/week-17-project/date.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #.... Initialization steps
-#$(date "+%Y-%m-%d %H:%M:%S:%Z") ##UTC TZ
+#$(date "+%Y-%m-%d %H:%M:%S:%Z") ##UTC TZ (default) FIND YOUR TIME ZONE ===> cd /usr/share/zoneinfo 
 echo Image Built: $(TZ=EST5EDT date "+%Y-%m-%d %H:%M:%S:%Z") > build-date.txt
 echo $(cat build-date.txt)
 #.... More initialization steps
